6a4db9daac757567dd7f37565c30d6c7e79bf4c6
Pavle Portic <pavle.portic@tilda.center>
2020-12-27 22:46:27 +0100

    Add kazan and twitter

--- kazan.tsx
+++ kazan.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="kazan" tilewidth="32" tileheight="32" tilecount="9" columns="3">
- <image source="../../../pictures/rc3/kazan.png" width="96" height="96"/>
+<tileset version="1.4" tiledversion="1.4.3" name="kazan" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+ <image source="tiles_png/kazan.png" width="128" height="128"/>
 </tileset>
